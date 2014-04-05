f36270a6091471009d48a3069a63bb5110850e8c
Tony Huang <ttonyhh@gmail.com>
2014-04-05 19:55:43 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TonyApp::Application.config.secret_key_base = 'd2f4c2b2ed3c8f0aa41873518bf183e60bea67a7c4f7ae4bbc686e4290e87af79dc4fc01ad818ba99c184b2efe2b1c5cfc2201a097db12b9c7fe6aabf65f34de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TonyApp::Application.config.session_store :cookie_store, key: '_tony_app_session'
